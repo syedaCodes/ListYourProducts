dfa67446550045fe3cd80b204d7686079b5cd8f8
syedaCodes <syedaa.codes@gmail.com>
2021-06-17 22:32:07 +0530

    styled form and table

---
+++ src/Components/Temp.js
@@ -0,0 +1,10 @@
+/* Temporary file to connect with unsplash.com */
+const API = 'url';
+
+import React from 'react';
+
+const Temp = () => {
+
+}
+
+export default Temp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './Components/Header';
+import Manage from './Components/Manage';
+import './assets/style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    
+    return(
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Switch>
+                        <Route path="/" exact />
+                        <Route path="/manage" component={Manage} exact />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#app'));
