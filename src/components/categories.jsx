381fa4ffe284fd4cabaa6f5f66c1c6bc1a29b2cd
nehacodes7 <neha.syeda@silcgroup.com>
2023-06-05 22:23:59 +0530

    updated categories and categories item component

---
+++ src/components/categories.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "./categoryItem";
+
+const Categories = ({ categories }) => {
+
+  return (
+    <div className="clothing">
+        {categories?.map((category) => (
+            <CategoryItem category={category?.title} key={category?.id} />
+        ))}
+    </div>
+  )
+}
+
+export default Categories;
---
+++ src/components/categoryItem.jsx
@@ -0,0 +1,15 @@
+const CategoryItem = ({ category }) => {
+
+    const categoryClass = category.toLowerCase();
+  
+    return (
+    <div className={`clothing__type ${categoryClass}`}>
+        <div className="clothing__type--category">
+            <h2 title={category}>{category}</h2>
+            <a href="/">Shop Now</a>
+        </div>
+    </div>
+  )
+}
+
+export default CategoryItem;
