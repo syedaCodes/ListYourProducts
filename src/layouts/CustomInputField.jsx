37950efb2abba767209e0dccbe1d81a7ba01934c
syedaCodes <syedaa.codes@gmail.com>
2023-06-18 18:20:47 +0530

    added sign in with email password and fixed styles

--- src/layouts/CustomInputField.jsx
+++ src/layouts/CustomInputField.jsx
@@ -2,7 +2,14 @@ const CustomInputField = ({ label, id, ...otherProps }) => {
     return (
         <div className="form-group">
             <input {...otherProps} />
-            {label && <label htmlFor={id}>{label}</label>}
+            {label && (
+                <label
+                    htmlFor={id}
+                    className={otherProps.value.length && "shrink"}
+                >
+                    {label}
+                </label>
+            )}
         </div>
     );
 };
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,30 +1,97 @@
 import { useNavigate } from "react-router-dom";
+import CustomInputField from "../../layouts/CustomInputField";
 import Button from "../../layouts/Button";
 import {
     createDocumentFromAuth,
+    signInUserAuthWithEmailandPassword,
     signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
+import { useState } from "react";
+
+const defaultFields = {
+    email: "",
+    password: "",
+};
 
 const SignIn = () => {
+    const [formFields, setFormFields] = useState(defaultFields);
+
+    const { email, password } = formFields;
+
     const navigate = useNavigate();
 
     const navigateToSignUp = () => navigate("/sign-up");
 
+    const resetFormFields = () => setFormFields(defaultFields);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!formFields) return;
+
+        try {
+            signInUserAuthWithEmailandPassword(email, password);
+            resetFormFields();
+        } catch (error) {
+            switch (error.code) {
+                case "auth/wrong-password":
+                    alert("incorrect password for email");
+                    break;
+
+                case "auth/user-not-found":
+                    alert("no user associated with this email");
+                    break;
+                default:
+                    console.log(error);
+            }
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormFields({ ...formFields, [name]: value });
+    };
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = await createDocumentFromAuth(user);
+        await createDocumentFromAuth(user);
     };
 
     return (
         <div className="signin">
             <h1>Sign In</h1>
-            <Button buttonType="btn-primary" onClick={logGoogleUser}>
-                Sign In With Google Popup
+            <form onSubmit={handleSubmit}>
+                <CustomInputField
+                    label="Email"
+                    id="email"
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={email}
+                    required
+                />
+                <CustomInputField
+                    label="Password"
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={password}
+                    required
+                />
+                <Button buttonType="btn-primary" type="submit">
+                    Submit
+                </Button>
+            </form>
+            <Button buttonType="btn-primary google-btn" onClick={logGoogleUser}>
+                Sign In With Google
             </Button>
-            <h2>Do not have an account?</h2>
-            <Button buttonType="btn-secondary" onClick={navigateToSignUp}>
-                Create Now
-            </Button>
+            <div className="signin__footer">
+                <h2>Do not have an account?</h2>
+                <Button buttonType="btn-secondary" onClick={navigateToSignUp}>
+                    Create Now
+                </Button>
+            </div>
         </div>
     );
 };
