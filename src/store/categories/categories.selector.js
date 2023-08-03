36ff6b2e314d8c06715c8705fa3a7c39d12e9311
syedaCodes <syedaa.codes@gmail.com>
2023-08-04 00:14:35 +0530

    using reselect for category selector

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,9 +1,21 @@
-export const selectorCategoriesMap = (state) =>
-    state?.categories?.categories.reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectorCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) =>
+        categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
+);
 
 // const cacheObj = {};
 // ---------------------------------------------------------- Waiting for review
