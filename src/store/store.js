fb2be6b20feaf0e34fa69516eb052a42cd9b6262
syedaCodes <syedaa.codes@gmail.com>
2023-08-18 23:55:11 +0530

    user reducer refactored for toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,44 +1,57 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import logger from "redux-logger";
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
-import thunk from "redux-thunk";
-// import createSagaMiddleware from "redux-saga";
-// import { rootSaga } from "./root-saga";
-
-// const sagaMiddleWare = createSagaMiddleware();
+import logger from "redux-logger";
 
 const middleWares = [
-    import.meta.env.VITE_APP_ENVIRONMENT !== "production" && logger,
-    thunk,
-    // sagaMiddleWare,
+    import.meta.env.VITE_APP_ENVIRONMENT === "development" && logger,
 ].filter(Boolean);
 
-const composeEnhancer =
-    (import.meta.env.VITE_APP_ENVIRONMENT !== "production" &&
-        window &&
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-    compose;
+export const store = configureStore({
+    reducer: rootReducer,
+    middleWares,
+});
 
-const persistConfig = {
-    key: "root",
-    storage,
-    blacklist: ["user"],
-    // whitelist: ["cart"],
-};
+// import { compose, createStore, applyMiddleware } from "redux";
+// // import { persistStore, persistReducer } from "redux-persist";
+// // import storage from "redux-persist/lib/storage";
+// // import logger from "redux-logger";
+// // import { rootReducer } from "./root-reducer";
+// // import thunk from "redux-thunk";
+// // import createSagaMiddleware from "redux-saga";
+// // import { rootSaga } from "./root-saga";
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// // const sagaMiddleWare = createSagaMiddleware();
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares)); //enhance our store
+// const middleWares = [
+//     import.meta.env.VITE_APP_ENVIRONMENT !== "production" && logger,
+//     // thunk,
+//     // sagaMiddleWare,
+// ].filter(Boolean);
 
-//action -> middleware runs before action hits -> reducers
-export const store = createStore(
-    persistedReducer,
-    undefined,
-    composedEnhancers
-); //undefined for an optional parameter - additional default state for testing made easy
+// const composeEnhancer =
+//     (import.meta.env.VITE_APP_ENVIRONMENT !== "production" &&
+//         window &&
+//         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//     compose;
 
-// sagaMiddleWare.run(rootSaga);
+// const persistConfig = {
+//     key: "root",
+//     storage,
+//     blacklist: ["user"],
+//     // whitelist: ["cart"],
+// };
 
-export const persistor = persistStore(store);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares)); //enhance our store
+
+// //action -> middleware runs before action hits -> reducers
+// export const store = createStore(
+//     persistedReducer,
+//     undefined,
+//     composedEnhancers
+// ); //undefined for an optional parameter - additional default state for testing made easy
+
+// // sagaMiddleWare.run(rootSaga);
+
+// export const persistor = persistStore(store);
